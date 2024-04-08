6bf6763d5b5e6e08ea17cabd449ef8ba3249fa7c
Naguib5 <naguibnaguib2018@gmail.com>
2024-04-08 18:35:05 +0200

    project files

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Chat App'),
+        ),
+      ),
+    );
+  }
+}
