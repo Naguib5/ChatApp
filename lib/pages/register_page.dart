403c973bb182cf4d2990f18cad5c2b591fbefe95
Naguib5 <naguibnaguib2018@gmail.com>
2024-04-17 01:01:26 +0200

    make validate in the code

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -9,109 +9,123 @@ class RegisterPage extends StatelessWidget {
   static const String id = 'RegisterPage';
   String? email;
   String? password;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        child: ListView(children: [
-          const SizedBox(
-            height: 100,
-          ),
-          Image.asset(
-            'assets/images/scholar.png',
-            height: 100,
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Chat',
-                  style: TextStyle(
-                      fontSize: 32,
-                      color: Colors.white,
-                      fontFamily: 'Pacifico')),
-            ],
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          const Row(
-            children: [
-              Text('Register',
-                  style: TextStyle(fontSize: 36, color: Colors.white)),
-            ],
-          ),
-          const SizedBox(
-            height: 26,
-          ),
-          CustomTextField(
-            onChanged: (data) {
-              email = data;
-            },
-            hintText: 'Email',
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            onChanged: (data) {
-              password = data;
-            },
-            hintText: 'Password',
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          CustomButton(
-            onPressed: () async {
-              try {
-                UserCredential user = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: email!, password: password!);
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'weak-password') {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text('The password provided is too weak.'),
-                  ));
-                } else if (e.code == 'email-already-in-use') {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text('The account already exists for that email.'),
-                  ));
-                }
-              }
-              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                content: Text('successfully registered.'),
-              ));
-            },
-            text: 'Register',
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('already have an account?',
-                  style: TextStyle(fontSize: 16, color: Colors.white)),
-              TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text(
-                    'Login',
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: ListView(children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Image.asset(
+              'assets/images/scholar.png',
+              height: 100,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Chat',
                     style: TextStyle(
-                      fontSize: 16,
-                      color: kSecondaryColor,
-                    ),
-                  ))
-            ],
-          )
-        ]),
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontFamily: 'Pacifico')),
+              ],
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            const Row(
+              children: [
+                Text('Register',
+                    style: TextStyle(fontSize: 36, color: Colors.white)),
+              ],
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            CustomTextField(
+              onChanged: (data) {
+                email = data;
+              },
+              hintText: 'Email',
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              onChanged: (data) {
+                password = data;
+              },
+              hintText: 'Password',
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            CustomButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  try {
+                    userRegister();
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'weak-password') {
+                      showSnackBar(
+                          context, "The password provided is too weak.");
+                    } else if (e.code == 'email-already-in-use') {
+                      showSnackBar(context,
+                          'The account already exists for that email.');
+                    }
+                  } catch (ex) {
+                    showSnackBar(
+                        context, 'there was an error please try again later.');
+                  }
+                  showSnackBar(context, 'successfully registered.');
+                }
+              },
+              text: 'Register',
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('already have an account?',
+                    style: TextStyle(fontSize: 16, color: Colors.white)),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: kSecondaryColor,
+                      ),
+                    ))
+              ],
+            )
+          ]),
+        ),
       ),
     );
+  }
+
+  void showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
+
+  Future<void> userRegister() async {
+    UserCredential user = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email!, password: password!);
   }
 }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -6,12 +6,24 @@ class CustomTextField extends StatelessWidget {
   Function(String)? onChanged;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      validator: (data) {
+        if (data!.isEmpty) {
+          return 'field is required';
+        }
+        return null;
+      },
       onChanged: onChanged,
       decoration: InputDecoration(
         enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
             borderSide: const BorderSide(color: Colors.white)),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Colors.white)),
+        errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Colors.red)),
         focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12),
             borderSide: const BorderSide(color: Colors.white)),
